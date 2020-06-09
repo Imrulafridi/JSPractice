3598db6165966045904ac7e11f509b006dca47e5
Imranullah Afridi <imrulafridi@gmail.com>
2020-06-10 00:25:43 +0500

    updated

---
+++ app.js
@@ -0,0 +1,30 @@
+// alert ('Welcome To My Page');
+
+// alert ('Error! Please Enter a Valid Password.');
+
+// alert ('Welcome to JS Land... \nHappy Coding! ');
+
+// alert ('Welcome to JS Land...');
+
+// alert ('HAppy Coding! \n')
+
+// var username ;
+// var myName = "imranullah";
+
+// var message = "Hello World";
+
+// alert (message);
+
+
+
+// var name = "Jhon Doe";
+// var age = "15 years old";
+// var edu = "certified Mobile application development"
+// alert(name);
+// alert (age);
+// alert (edu);
+
+var food;
+food = "PIZZA";
+
+alert (food);
