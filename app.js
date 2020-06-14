cb9c96ea2b612d57225f5736e2cf688ea2fc2e7c
Imranullah Afridi <imrulafridi@gmail.com>
2020-06-15 01:15:59 +0500

    Newly Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,56 @@
+
+// ======= CHAPTER # 1 =======
+
 // alert ('Welcome To My Page');
-
 // alert ('Error! Please Enter a Valid Password.');
-
 // alert ('Welcome to JS Land... \nHappy Coding! ');
-
 // alert ('Welcome to JS Land...');
-
 // alert ('HAppy Coding! \n')
+// console.log("Hello... I can run JS through my web browser's console");
 
-// var username ;
-// var myName = "imranullah";
 
+
+// ======= CHAPTER # 2 =======
+
+// var username;
+// var myName = "Imranullah Afridi";
 // var message = "Hello World";
-
 // alert (message);
-
-
-
 // var name = "Jhon Doe";
 // var age = "15 years old";
 // var edu = "certified Mobile application development"
 // alert(name);
 // alert (age);
 // alert (edu);
+// var email = "imrulafridi@gmail.com"
+// alert("My email address is" + " " + email)
+// var book =  "A smarter way to learn JavaScript"
+// alert(book)
+// document.write("Yah! I can write HTML content through JavaScript")
+// var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(a);
 
-var food;
-food = "PIZZA";
 
-alert (food);
+
+// ======= CHAPTER # 3 =======
+
+// var age = 27;
+// alert('I am ' + age + ' years old' )
+// var num = 14;
+// alert('You have visited this site ' + num + ' times ')
+// var birthYear = 1992;
+// document.write('My birth year is ' + birthYear + '\n Date type of my declared variable is number' );
+// var name = prompt('Enter Your Name')
+// var prod = prompt('Enter Your Product')
+// var qty = prompt('Enter Quantity')
+// document.write( name + " ordered " + qty + prod + " on Daraz.pk"  )
+
+
+
+// ======= CHAPTER # 4 =======
+
+
+
+
+
+
