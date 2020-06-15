50fe879c25896784aa5d30e57e40e0318fcd1e71
Imranullah Afridi <imrulafridi@gmail.com>
2020-06-15 23:51:47 +0500

    app.js updated

--- app.js
+++ app.js
@@ -49,7 +49,287 @@
 
 // ======= CHAPTER # 4 =======
 
+// var a = imran, b = 25, c = true;
 
+// // Legal Names
+// var imran;
+// var $imran;
+// var _imran;
+// var imranUllah;
+// var num1;
+
+// // Illegal Names
+// var imran ulla;
+// var 1imran;
+// var /imran;
+// var ?imran;
+// var !imran;
+
+
+// document.write(" <b> Rules for naming JS variables </b> <br> ")
+// document.write(" <br> Rules for naming JS variables  ")
+// document.write(" <br>  Variable names can only contain letter, numbers, $ and _.  For example $my_1stVariable ")
+// document.write(" <br> Variables must begin with a letter, $ or _. For example $name, _name or name ")
+// document.write(" <br> Variable names are case sensitive  ")
+// document.write(" <br> Variable names should not be JS Keywords  ")
+
+
+
+
+// ======= CHAPTER # 5 =======
+
+// var a = prompt(" Enter first number ")
+// var b = prompt(" Enter second number ")
+// var c = Number(a) + Number(b);
+// document.write( "Sum of" + " " + a + " " + "and" + " " +  b + " " + " is " + c )
+
+// var a = prompt(" Enter first number ")
+// var b = prompt(" Enter second number ")
+// var c = Number(a) - Number(b);
+// document.write( "Subtraction of" + " " + a + " " + "and" + " " +  b + " " + " is " + c )
+
+// var a = prompt(" Enter first number ")
+// var b = prompt(" Enter second number ")
+// var c = Number(a) * Number(b);
+// document.write( "Multiply of" + " " + a + " " + "and" + " " +  b + " " + " is " + c )
+
+// var a = prompt(" Enter first number ")
+// var b = prompt(" Enter second number ")
+// var c = Number(a) / Number(b);
+// document.write( "Division of" + " " + a + " " + "and" + " " +  b + " " + " is " + c )
+
+
+// var a;
+// document.write(" Value after variable declaration is " + a)
+// a = 5;
+// document.write(" <br>  Initial value: " + a)
+// a = ++a;
+// document.write(" <br>  Value After Increment is " + a);
+// a = 7+a;
+// document.write(" <br>  Value After Addition is " + a);
+// a = --a;
+// document.write(" <br>  Value After decrement is " + a);
+// a = a / 3 - 4;
+// document.write(" <br>  The Remainder is " + a);
+
+// var price = 600;
+// var ticket = 5;
+// var total = price * ticket;
+// document.write( " Total cost to buy " + " " + ticket + " " + " tickets to a movie is " + " " +  total + "PKR" )
+
+// document.write( "Table of 4")
+// document.write( "<br>" + 4 + " X " + 1 + " = " + 4 )
+// document.write( "<br>" + 4 + " X " + 2 + " = " + 8 )
+// document.write( "<br>" + 4 + " X " + 3 + " = " + 12 )
+// document.write( "<br>" + 4 + " X " + 4 + " = " + 16 )
+// document.write( "<br>" + 4 + " X " + 5 + " = " + 20 )
+// document.write( "<br>" + 4 + " X " + 6 + " = " + 24 )
+// document.write( "<br>" + 4 + " X " + 7 + " = " + 28 )
+// document.write( "<br>" + 4 + " X " + 8 + " = " + 32 )
+// document.write( "<br>" + 4 + " X " + 9 + " = " + 36 )
+// document.write( "<br>" + 4 + " X " + 10 + " = " + 40 )
+
+// document.write(" <H1> Shopping Cart </H1> ")
+// var item1 = 650;
+// document.write(" <br> Price of item 1 is " + item1)
+// var qty1 = 3;
+// document.write(" <br> Quantity of item 1 is " + qty1)
+// var total1 = item1 * qty1;
+// var item2 = 100;
+// document.write(" <br> Price of item 2 is " + item2)
+// var qty2 = 7;
+// document.write(" <br> Quantity of item 2 is " + qty2)
+// var total2 = item1 * qty1;
+// var total = Number(total1) + Number(total2);
+// document.write(" <br> Shipping Charges 100 ")
+// document.write(" <br> <br> <br> Total cost of your order is " + total)
+
+// document.write(" <H1> Marks Sheet </H1>  ")
+// var totalNum = 800;
+// document.write(" <br> Total Marks : " + totalNum)
+// var marks = 741;
+// document.write( " <br> Marks obtained: " + marks  )
+// var percent = marks / totalNum ;
+// document.write(" <br> Percentage : " + percent * 100)
+
+
+
+// ======= CHAPTER # 6 To 9 =======
+
+// var a = prompt("Enter Your Number")
+// document.write("Result <br>")
+// document.write("The Value of a is: " + a + "<br>")
+// document.write(" <br> The value of ++a is : " + ++a)
+// document.write(" <br> Now the value of a is : " + a + "<br>")
+// document.write(" <br> The value of a++ is : " + a++)
+// document.write(" <br> Now the value of a is : " + a + "<br>")
+// document.write(" <br> The value of --a is : " + --a )
+// document.write(" <br> Now the value of a is : " + a + "<br>")
+// document.write(" <br> The value of a-- is : " + a--)
+// document.write(" <br> Now the value of a is : " + a)
+
+// var user = prompt("Enter Your Name")
+// alert("Welcome " + user)
+
+
+
+// ======= CHAPTER # 9 To 11 =======
+
+// var city = prompt("Enter Your City Name")
+
+// if(city === "karachi"){
+
+//   alert("Welocome To the city of Light")
+// }
+// else{
+//   alert( "Enter a valid city name" )
+// }
+
+
+// var gender = prompt("Enter Your Gender")
+// if(gender === "male"){
+//   alert("Good Morning Sir")
+// }
+// else if(gender === "female"){
+//   alert("Good Morning Ma'am")
+// }
+// else{
+//   alert("Enter Valid Gender")
+// }
+
+// var fuel = prompt("Whats your fuel liter?")
+// if(fuel <= .25){
+//   alert("Please Refill The Fuel in your car")
+// }
+// else{
+//   alert("You have enough fuel ")
+// }
+
+// var guess = prompt("Guess a right number")
+// var num = 3;
+// if(guess == num){
+//   alert("Bingo! Correct answer")
+// }
+// else if (num = ++guess){
+//   alert("Close Enough");
+// }
+// else(
+//     alert("Wrong Answer")
+// )
+
+// var user = prompt("Enter Your Number")
+// var num = user%3;
+// if (num === 0 ){
+//   alert("Your number is divisible by 3") 
+// }
+// else{
+//   alert("Not Divisible by 3")
+// }
+
+// var user = prompt("Enter Your Number")
+// var num = user % 2;
+// if (num === 0){
+//     alert("Your number is even") 
+//   }
+//   else{
+//     alert("Your number is odd") 
+// }
+
+
+
+// ======= CHAPTER # 12 To 13 =======
+
+// var num1 = prompt("Enter First number")
+// var num2 = prompt("Enter Second number")
+// if(num1 > num2){
+//   alert("First Number is greater than second")
+// }
+// else if(num1 < num2){
+//   alert("First number is less than second")
+// }
+// else{
+//   alert("Both are equal")
+// }
+
+
+// var num = prompt("Enter Your number")
+// if(num > 0){
+//     alert("Your number is positive")
+//   }
+//   else if(num < 0){
+//     alert("Your number is negative")
+//   }
+//   else{
+//     alert("your number is zero")
+//   }
+
+
+// var char = prompt("Enter a character")
+// if (char === "a"){
+//   alert("Your character is vowel")
+// }
+// else if (char === "e"){
+//   alert("Your character is vowel")
+// }
+// else if (char === "i"){
+//   alert("Your character is vowel")
+// }
+// else if (char === "o"){
+//   alert("Your character is vowel")
+// }
+// else if (char === "u"){
+//   alert("Your character is vowel")
+// }
+// else{
+//   alert("Your character is not vowel")
+// }
+
+// var pass = "imran";
+// var user = prompt("Enter Your Password")
+// if (pass === user){
+//   alert("Your Password is Correct")
+// }
+// else if(pass != user){
+//   alert("Incorrect Password")
+// }
+// else if(user === " "){
+//   alert("Enter Password Please")
+// }
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";}
+// else{
+// greeting = "Good evening";
+// }
+
+
+
+
+// ======= CHAPTER # 14 To 16 =======
+
+// var student = []
+// var arr = ["imran", "atif", "sohail" ]
+// var arrNum = [1 ,2 , 3, 4, ]
+// var arrBolean = [true, false ]
+// var mixarr = [1, "imran", "jon doe", true ]
+// var edu = [ "SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phill", "PhD" ]
+// document.write(" <H1> Qualifications: </H1>")
+// document.write("<br>", "1)", edu[0],"<br>", "2)", edu[1],"<br>", "3)", edu[2],"<br>", "4)", edu[3],"<br>", "5)", edu[4],"<br>", "6)", edu[5],"<br>", "7)", edu[6],"<br>", "8)", edu[7], )
+
+
+
+
+// ======= CHAPTER # 17 To 18 =======
+
+// var arr = [ ["a" , "b"], ["c" , "d" ], [ "e" , "f" ] ]
+// var arr = [ [0 ,1 ,2 ,3], "<br>", [1,0,1,2], "<br>", [2,1,0,1] ]
+// document.write(arr)
+
+// for(var i = 0; i <= 10; i++ ){
+//   document.write(i, "<br>" )
+// }
 
 
 
